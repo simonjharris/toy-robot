8fe458e1057ec2ae2aa3be870933ac7b3d658e3c
Simon Harris <simonjharris90@gmail.com>
2026-02-05 09:49:05 +0000

    Add __eq__ for Point for easier comparison.

--- src/data_classes.py
+++ src/data_classes.py
@@ -13,3 +13,11 @@ class Direction(StrEnum):
 class Point:
     x: int
     y: int
+
+    def __eq__(self, other: "Point | tuple[int, int]") -> bool:
+        if isinstance(other, tuple):
+            if len(other) != 2:
+                return False
+            return self.x == other[0] and self.y == other[1]
+        else:
+            return self.x == other.x and self.y == other.y
